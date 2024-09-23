1fbd098aec7de03e33fac5e526fd7654cf1aa8df
Luan Silva <luanncostas@gmail.com>
2024-09-23 14:38:29 -0300

    commit server

--- Lab64/server.js
+++ Lab64/server.js
@@ -6,11 +6,11 @@ const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end('<h1>Pagina inicial!</h1><p>Esta é a página inicial.</p>');
+        res.end('<h1>Pagina inicial!</h1><p>Uma página inicial de um site deve conter informações claras e relevantes, que facilitem a navegação e a compreensão do que o site oferece</p>');
     } else if (req.url === '/about') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end('<h1>Sobre</h1><p>Esta é a página sobre.</p>');
+        res.end('<h1>Sobre</h1><p>O conteúdo de uma página sobre pode variar de acordo com o tipo de página</p>');
     } else if (req.method === 'POST' && req.url === '/upload') {
         let fileData = '';
         req.on('data', chunk => {
